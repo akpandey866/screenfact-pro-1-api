9a0c002f8b2c34564d9a37daa99fce695da31203
priyanka1093 <151918993+priyanka1093@users.noreply.github.com>
2023-12-02 14:59:01 +0530

    changes

--- index.js
+++ index.js
@@ -3,12 +3,18 @@
 const express = require('express');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 require("./configs/db.js");
 app.use(bodyParser.json());
-const port = 3000;
+const port = 3001;
+
+app.use(cors());
 
 app.use('/', routes);
+//app.use(cors());
+
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
